d5d8eab4ce4bcfff25431a1dcc2bf822e09b5b3a
Angela Zhang <angelazhang117@gmail.com>
2020-07-10 15:28:17 -0400

    Updates

---
+++ precache-manifest.8365f9073dd7964e76a0337388997c09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5818db772a45b7446b43f0c5abd202bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31769c7a54dbf0fe1f90",
+    "url": "/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "27e3c6cf66802111095d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "31769c7a54dbf0fe1f90",
+    "url": "/static/js/2.8fc282df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fc282df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27e3c6cf66802111095d",
+    "url": "/static/js/main.0f166a72.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
